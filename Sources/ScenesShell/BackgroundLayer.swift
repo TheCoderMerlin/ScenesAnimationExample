3c8eae4f79c8c4785da23b543f0a071a100010b1
Camden Thomson <camden.thomson@student.allenisd.org>
2020-04-26 15:28:45 -0500

    Implemented Animation Updates including loop, reverse, and inverse functionality

--- Sources/ScenesShell/BackgroundLayer.swift
+++ Sources/ScenesShell/BackgroundLayer.swift
@@ -1,10 +1,19 @@
 import Scenes
 
+/*
+ This class is responsible for the background Layer.
+ Internally, it maintains the RenderableEntities for this layer.
+ */
+
+
 class BackgroundLayer : Layer {
     let background = Background()
 
     init() {
+        // Using a meaningful name can be helpful for debugging
         super.init(name:"Background")
+
+        // We insert our RenerableEntities in the constructor
         insert(entity:background, at:.back)
     }
 }
--- Sources/ScenesShell/MovingRectangle.swift
+++ Sources/ScenesShell/MovingRectangle.swift
@@ -2,39 +2,39 @@ import Igis
 import Scenes
 
 class MovingRectangle : RenderableEntity {
+    let fillStyle : FillStyle
     let rectangle : Rectangle
-    let fill : FillStyle
     let text : Text
-    let ease : EasingStyle
+
+    var ease : EasingStyle
     var animation : Animation? = nil
 
     init(yPos:Int, color:Color, label:String, ease:EasingStyle) {
+        fillStyle = FillStyle(color:color)
         rectangle = Rectangle(rect:Rect(topLeft:Point(x:200, y:120+(yPos*40)), size:Size(width:100, height:30)), fillMode:.fill)
-        fill = FillStyle(color:color)
         text = Text(location:Point(x:80, y:rectangle.rect.centerY), text:label)
         text.alignment = .center
         text.baseline = .middle
         text.font = "20px Calibri"
         self.ease = ease
 
-        super.init(name:"MovingRectangle")
+        // Using a meaningful name can be helpful for debugging
+        super.init(name:"MovingRectangle")        
     }
 
     override func setup(canvasSize:Size, canvas:Canvas) {
         let fromPoint = rectangle.rect.topLeft
         let toPoint = Point(x:canvasSize.width - 200, y:rectangle.rect.topLeft.y)
-        
         let tween = Tween(from:fromPoint, to:toPoint, duration:3, ease:ease, update: {self.rectangle.rect.topLeft = $0})
-        animation = Animation(tween:tween)
         
-        animationManager.run(animation:animation!)
-    }
-
-    func replay() {
+        animation = Animation(tween:tween)
+        animation!.reverse = true
+        animation!.loop = true
+        
         animationManager.run(animation:animation!)
     }
 
     override func render(canvas:Canvas) {
-        canvas.render(fill, rectangle, text)
+        canvas.render(fillStyle, rectangle, text)
     }
 }
